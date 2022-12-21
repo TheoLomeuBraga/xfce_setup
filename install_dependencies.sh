c21e3bd6469bce8222ffb44493f6bd04299c8341
TheoLomeuBraga <theolomeu@gmail.com>
2022-12-21 10:57:54 -0300

    a

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,13 +1,2 @@
-sudo add-apt-repository ppa:xuzhen666/dockbarx
-sudo apt-get -y update
-sudo apt -y install xfce4-dockbarx-plugin
-sudo apt -y install appmenu-gtk-module-common 
-sudo apt -y install appmenu-gtk2-module 
-sudo apt -y install appmenu-gtk3-module 
-sudo apt -y install appmenu-registrar 
-sudo apt -y install bamfdaemon 
-sudo apt -y install libappmenu-gtk2-parser0
-sudo apt -y install libappmenu-gtk3-parser0
-sudo apt -y install libdbusmenu-gtk4 
-sudo apt -y install vala-panel-appmenu-common
-sudo apt -y install xfce4-appmenu-plugin
+sudo apt install openbox thunar firefox network-manager pnmixer
+sudo apt install openbox tint2 rofi
